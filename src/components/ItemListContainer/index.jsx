b8554c85f98bc2f31d0aae8c7ddc3ea9e13d1997
Rodrigo Gutierrez <rodrigogutierrezh4@gmail.com>
2022-09-22 07:22:55 -0500

    Navbar, CartWidget y ItemList con styling integrado

---
+++ src/components/ItemListContainer/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from '../Title'
+
+export const ItemListContainer = () => {
+    return (
+    <Title greeting="Tus bebidas favoritas, a tu alcance." />
+    );
+}
+
+export default ItemListContainer;
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,31 +1,46 @@
+import { buildQueries } from "@testing-library/react";
 import React from "react";
+import CartWidget from "../CartWidget";
+import './navbar.css'
+
+const navbarbg = {
+  backgroundColor: '#3E6BDD'
+}
+
+const logo = {
+  width: '120px',
+  height: '40px',
+  transform: 'translateX(20px)'
+
+}
+
 
 export const NavBar = () => {
     return (
-        <nav class="navbar navbar-expand-lg na">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img className="logo" src="/img/refreshme2-cropped.svg"></img>
+        <nav className="navbar navbar-expand-lg na" style={navbarbg}>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="img/refreshme2-cropped.svg" style={logo}/>
           </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Catálogo</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Catálogo</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Nosotros</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Nosotros</a>
               </li>
-              <li class="nav-item">
-              <a class="nav-link" href="#">Nosotros</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#"><CartWidget/></a>
               </li>
-
-             
+              <li className="nav-item">
+              </li>     
             
             </ul>
           </div>
